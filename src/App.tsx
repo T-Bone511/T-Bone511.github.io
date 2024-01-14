c2ecd5c69473b886c5f11ee9c629e9531a0ca52f
T-Bone511 <129640384+T-Bone511@users.noreply.github.com>
2024-01-13 22:30:19 -0500

    RouterUpdate
    Router and pages added

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,55 @@
-import './App.css';
-import logo from "./assets/logo.png"
-import WEEK1 from "./assets/WEEK1.png"
+import "./App.css";
+import logo from "./assets/logo.png";
+import WEEK1 from "./assets/WEEK1.png";
+import React, { useState } from "react";
 
 function App() {
+  const [page, setPage] = useState("home");
+
+  function handlePageChange(newPage: string) {
+    setPage(newPage);
+  }
+
   return (
     <div className="page">
-
       <div className="sideBar">
-        <img src={logo} alt="" />
+        <img src={logo} alt="" onClick={() => handlePageChange("home")} />
         T-Bone's
         <div></div>
         52 Weeks of creation
       </div>
 
-      <div className="box">
-
-      </div>
+      <div className="box"></div>
 
       <div className="content">
+        {page === "home" && (
+          <>
+            <img
+              className="projectLink"
+              src={WEEK1}
+              alt="Week1"
+              onClick={() => handlePageChange("Week1")}
+            />
+            ​​
+            <button
+              className="projectLink"
+              onClick={() => handlePageChange("Week2")}
+            ></button>
+            <button className="projectLink"></button>
+            <button className="projectLink"></button>
+            <button className="projectLink"></button>
+            <button className="projectLink"></button>
+            <button className="projectLink"></button>
+            <button className="projectLink"></button>
+            <button className="projectLink"></button>
+            <button className="projectLink"></button>
+            <button className="projectLink"></button>
+          </>
+        )}
 
-        <img src={WEEK1} alt="Week1" />​​
-        <button className="projectLink"></button>
-        <button className="projectLink"></button>
-        <button className="projectLink"></button>
-        <button className="projectLink"></button>
-        <button className="projectLink"></button>
-        <button className="projectLink"></button>
-        <button className="projectLink"></button>
-        <button className="projectLink"></button>
-        <button className="projectLink"></button>
-        <button className="projectLink"></button>
+        {page === "Week1" && <div>Week1</div>}
+        {page === "Week2" && <div>Week2</div>}
       </div>
-
-
     </div>
   );
 }
